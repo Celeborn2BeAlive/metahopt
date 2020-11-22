c63b26b953c7ea5307a78e52f6395ac7cc78264a
Alexandre Marty <alexandre@marty.in>
2020-11-22 19:50:44 +0100

    Improved typing in LocalSearch and added termination reason

--- src/metahopt/local_search.py
+++ src/metahopt/local_search.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass, fields
 from enum import Enum
 import logging
 from time import process_time
-from typing import cast, Sequence, Tuple, Generic, List, Optional, Union
+from typing import Callable, cast, Sequence, Tuple, Generic, List, Optional, Union
 
 from metahopt.scoring import ScoringResults, score_solutions, score_vectorized
 from metahopt.typing import (
@@ -34,9 +34,16 @@ class PollOrder(Enum):
     random = "random"
 
 
+class TerminationReason(Enum):
+    max_time = 0
+    max_iter = 1
+    max_calls = 2
+    min_score = 3
+
+
 @dataclass
 class LocalSearchState(Generic[SolutionType]):
-    params: "LocalSearch"
+    params: LocalSearch
     best_score: float
     best_solution: SolutionType
     time: float
@@ -46,7 +53,7 @@ class LocalSearchState(Generic[SolutionType]):
     success_direction: Optional[int]
 
     @classmethod
-    def from_base_state(cls, state: "LocalSearchState", *args, **kwargs):
+    def from_base_state(cls, state: LocalSearchState, *args, **kwargs):
         # TODO: Metaclass to assign child's class signature?
         unpacked = tuple(getattr(state, field.name) for field in fields(state))
         return cls(*unpacked, *args, **kwargs)
@@ -77,13 +84,23 @@ class LocalSearch(metaclass=ABCMeta):
         self.poll_order = PollOrder(self.poll_order)
         self._logger = logging.getLogger("metahopt.solver")
 
+        # Type hints
+        self._score_func_iter = cast(ScoreFunc, self.score_func)
+        self._score_func_vec = cast(VectorizedScoreFunc, self.score_func)
+        self._neighborhood_func_vec = cast(
+            Callable[[LocalSearchState], Sequence[SolutionType]],
+            self.get_polling_set,
+        )
+        self._neighborhood_func_iter = cast(
+            Callable[[LocalSearchState], SizedIterable[SolutionType]],
+            self.get_polling_set,
+        )
+
     def init_state(self, starting_point: SolutionType) -> LocalSearchState:
         if self.vectorized:
-            score_func_vec = cast(VectorizedScoreFunc, self.score_func)
-            init_score = score_func_vec([starting_point])[0]
+            init_score = self._score_func_vec([starting_point])[0]
         else:
-            score_func = cast(ScoreFunc, self.score_func)
-            init_score = score_func(starting_point)
+            init_score = self._score_func_iter(starting_point)
         return LocalSearchState(
             params=self,
             best_score=init_score,
@@ -100,39 +117,29 @@ class LocalSearch(metaclass=ABCMeta):
     ) -> Union[SizedIterable[SolutionType], Sequence[SolutionType]]:
         """Generate neighborhood."""
 
-    def score(
-        self,
-        state: LocalSearchState,
-        polling_set: Union[SizedIterable[SolutionType], Sequence[SolutionType]],
+    def score_iter(
+        self, state: LocalSearchState, polling_set: SizedIterable[SolutionType]
     ) -> ScoringResults:
         random_order = self.poll_order is PollOrder.random
-        if self.vectorized:
-            # Typing hints, deactivated for performance
-            # score_func = cast(VectorizedScoreFunc, self.score_func)
-            # polling_set = cast(Sequence[SolutionType], polling_set)
-            return score_vectorized(
-                self.score_func,  # type: ignore
-                polling_set,  # type: ignore
-                random_order,
-                self.rng_seed,
-            )
-        else:
-            # Typing hints, deactivated for performance
-            # score_func = cast(ScoreFunc, self.score_func)
-            # polling_set = cast(SizedIterable[SolutionType], polling_set)
-            max_time = None if self.max_time is None else self.max_time - state.time
-            max_eval = (
-                None if self.max_calls is None else self.max_calls - state.n_calls
-            )
-            return score_solutions(
-                self.score_func,  # type: ignore
-                polling_set,  # type: ignore
-                max_time,
-                max_eval,
-                stop_score=None if self.complete_poll else state.best_score,
-                random_order=random_order,
-                rng_seed=self.rng_seed,
-            )
+        max_time = None if self.max_time is None else self.max_time - state.time
+        max_eval = None if self.max_calls is None else self.max_calls - state.n_calls
+        return score_solutions(
+            self._score_func_iter,
+            polling_set,
+            max_time,
+            max_eval,
+            stop_score=None if self.complete_poll else state.best_score,
+            random_order=random_order,
+            rng_seed=self.rng_seed,
+        )
+
+    def score_vectorized(
+        self, state: LocalSearchState, polling_set: Sequence[SolutionType]
+    ) -> ScoringResults:
+        random_order = self.poll_order is PollOrder.random
+        return score_vectorized(
+            self._score_func_vec, polling_set, random_order, self.rng_seed
+        )
 
     def update(
         self, state: LocalSearchState, scoring_res: ScoringResults, start_time: float
@@ -155,26 +162,26 @@ class LocalSearch(metaclass=ABCMeta):
             success_direction=success_direction,
         )
 
-    def terminated(self, state: LocalSearchState) -> bool:
+    def check_termination(self, state: LocalSearchState) -> Optional[TerminationReason]:
         if self.min_score is not None and state.best_score < self.min_score:
             self._logger.debug("Stopping: reached score limit (%s)", self.min_score)
-            return True
+            return TerminationReason.min_score
         if self.max_time is not None and state.time > self.max_time:
             self._logger.debug("Stopping: reached time limit (%s)", self.max_time)
-            return True
+            return TerminationReason.max_time
         if self.max_iter is not None and state.n_iter >= self.max_iter:
             self._logger.debug("Stopping: reached max steps (%s)", self.max_iter)
-            return True
+            return TerminationReason.max_iter
         if self.max_calls is not None and state.n_calls >= self.max_calls:
             self._logger.debug(
                 "Stopping: reached max score function calls (%s)", self.max_calls
             )
-            return True
-        return False
+            return TerminationReason.max_calls
+        return None
 
     def solve(
         self, starting_point: SolutionType
-    ) -> Tuple[LocalSearchState, List[ScoringResults]]:
+    ) -> Tuple[LocalSearchState, TerminationReason, List[ScoringResults]]:
         self._logger.info(
             "Minimizing %r with %s", self.score_func, self.__class__.__name__
         )
@@ -182,16 +189,21 @@ class LocalSearch(metaclass=ABCMeta):
         start_time = process_time()
 
         state = self.init_state(starting_point)
-        while not self.terminated(state):
-            # Type casting deactivated for polling_set for performance
-            polling_set = self.get_polling_set(state)  # type: ignore
-            scoring_res = self.score(state, polling_set)
+        termination_reason = self.check_termination(state)
+        while termination_reason is None:
+            if self.vectorized:
+                neighborhood_vec = self._neighborhood_func_vec(state)
+                scoring_res = self.score_vectorized(state, neighborhood_vec)
+            else:
+                neighborhood_iter = self._neighborhood_func_iter(state)
+                scoring_res = self.score_iter(state, neighborhood_iter)
             stats.append(scoring_res)
             state = self.update(state, scoring_res, start_time)
+            termination_reason = self.check_termination(state)
 
         n_step = 0 if state.n_iter is None else state.n_iter + 1
         self._logger.info(
             "Finished solving in %s steps (best_score=%s, best_solution=%s)",
             *(n_step, state.best_score, state.best_solution),
         )
-        return state, stats
+        return state, termination_reason, stats
--- tests/unit_tests/test_local_search.py
+++ tests/unit_tests/test_local_search.py
@@ -6,7 +6,12 @@ from metahopt.scoring import ScoringResults, ScoringStopReason
 from pytest_mock import MockerFixture
 
 import metahopt.local_search as mod
-from metahopt.local_search import PollOrder, LocalSearch, LocalSearchState
+from metahopt.local_search import (
+    PollOrder,
+    LocalSearch,
+    LocalSearchState,
+    TerminationReason,
+)
 
 
 class MyLocalSearch(LocalSearch):
@@ -33,7 +38,6 @@ def test_local_search_state():
 def test_local_search_init():
     solver = MyLocalSearch(score_func, poll_order=PollOrder.success)
     assert solver.poll_order is PollOrder.success
-
     assert isinstance(solver._logger, logging.Logger)
     assert solver._logger.name == "metahopt.solver"
 
@@ -43,25 +47,25 @@ def test_local_search_init():
 
 def test_local_search_init_state():
     solver = MyLocalSearch(lambda x: 42)
-    assert solver.init_state(12) == LocalSearchState(solver, 42, 12, 0, 0, 1, None)
+    assert solver.init_state("s") == LocalSearchState(solver, 42, "s", 0, 0, 1, None)
 
     solver = MyLocalSearch(lambda x: [42], vectorized=True)
-    assert solver.init_state(12) == LocalSearchState(solver, 42, 12, 0, 0, 1, None)
+    assert solver.init_state("s") == LocalSearchState(solver, 42, "s", 0, 0, 1, None)
 
 
 def test_local_search_score_vectorized(mocker: MockerFixture):
-    m_score_vectorized = mocker.patch.object(mod, "score_vectorized")
+    m_score_vec = mocker.patch.object(mod, "score_vectorized")
 
     solver = MyLocalSearch(score_func, vectorized=True, rng_seed=rng_seed)
-    assert solver.score(state, polling_set) is m_score_vectorized.return_value
-    m_score_vectorized.assert_called_once_with(score_func, polling_set, False, rng_seed)
+    assert solver.score_vectorized(state, polling_set) is m_score_vec.return_value
+    m_score_vec.assert_called_once_with(score_func, polling_set, False, rng_seed)
 
     mocker.resetall()
     solver = MyLocalSearch(
         score_func, vectorized=True, poll_order=PollOrder.random, rng_seed=rng_seed
     )
-    assert solver.score(state, polling_set) is m_score_vectorized.return_value
-    m_score_vectorized.assert_called_once_with(score_func, polling_set, True, rng_seed)
+    assert solver.score_vectorized(state, polling_set) is m_score_vec.return_value
+    m_score_vec.assert_called_once_with(score_func, polling_set, True, rng_seed)
 
 
 def test_local_search_score_not_vectorized(mocker: MockerFixture):
@@ -78,7 +82,7 @@ def test_local_search_score_not_vectorized(mocker: MockerFixture):
         complete_poll=True,
         rng_seed=rng_seed,
     )
-    assert solver.score(state, polling_set) is m_score_solutions.return_value
+    assert solver.score_iter(state, polling_set) is m_score_solutions.return_value
     m_score_solutions.assert_called_once_with(
         score_func,
         polling_set,
@@ -116,30 +120,56 @@ def test_local_search_update(mocker: MockerFixture):
     )
 
 
-def test_local_search_terminated():
+def test_local_search_check_termination():
     base_solver = MyLocalSearch(score_func)
     state = LocalSearchState(
         base_solver, 1.0, "sol", time=3, n_iter=4, n_calls=5, success_direction=None
     )
-    assert base_solver.terminated(state) is False
+    assert base_solver.check_termination(state) is None
 
     solver = replace(base_solver, min_score=2)
-    assert solver.terminated(replace(state, params=solver)) is True
+    assert (
+        solver.check_termination(replace(state, params=solver))
+        is TerminationReason.min_score
+    )
 
     solver = replace(base_solver, max_time=2)
-    assert solver.terminated(replace(state, params=solver)) is True
+    assert (
+        solver.check_termination(replace(state, params=solver))
+        is TerminationReason.max_time
+    )
 
     solver = replace(base_solver, max_iter=3)
-    assert solver.terminated(replace(state, params=solver)) is True
+    assert (
+        solver.check_termination(replace(state, params=solver))
+        is TerminationReason.max_iter
+    )
 
     solver = replace(base_solver, max_calls=3)
-    assert solver.terminated(replace(state, params=solver)) is True
+    assert (
+        solver.check_termination(replace(state, params=solver))
+        is TerminationReason.max_calls
+    )
 
 
-def test_local_search_solve(mocker: MockerFixture):
+def test_local_search_solve_iter(mocker: MockerFixture):
     def score_func(x):
         return dict(sol0=3, sol1=1, sol2=2)[x]
 
     solver = MyLocalSearch(score_func, max_iter=1)
-    end_state, stats = solver.solve("sol0")
+    end_state, term_reason, stats = solver.solve("sol0")
     assert end_state == LocalSearchState(solver, 1, "sol1", mocker.ANY, 1, 3, 0)
+    assert term_reason is TerminationReason.max_iter
+    assert len(stats) == 1
+
+
+def test_local_search_solve_vectorized(mocker: MockerFixture):
+    def score_func(x):
+        d = dict(sol0=3, sol1=1, sol2=2)
+        return [d[v] for v in x]
+
+    solver = MyLocalSearch(score_func, vectorized=True, max_iter=1)
+    end_state, term_reason, stats = solver.solve("sol0")
+    assert end_state == LocalSearchState(solver, 1, "sol1", mocker.ANY, 1, 2, 0)
+    assert term_reason is TerminationReason.max_iter
+    assert len(stats) == 1
