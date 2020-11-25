f7d524b6267bd0d220abe40f2b47b04546d86d34
Alexandre Marty <alexandre@marty.in>
2020-11-25 18:32:07 +0100

    Simplified example functions

--- src/metahopt/examples.py
+++ src/metahopt/examples.py
@@ -1,29 +1,50 @@
-from typing import Union
-
+"""Examples of objective functions.
+"""
 import numpy as np
 
-from metahopt.typing import ArrayLike, Scalar
+from metahopt.typing import ArrayLike
 
 
-def square_norm(x: Union[ArrayLike, Scalar]) -> np.ndarray:
-    """Squared distance from 0.
+# Numerical problems
+
+
+def square_norm(x: ArrayLike) -> np.ndarray:
+    """Squared distance from 0, vectorized on axis 0.
 
     Args:
-        x (np.ndarray): Array of dimension at least 2. Axis 0 holds solutions, other
-            axes are for the solution dimensions.
+        x (ArrayLike): Array of dimension at least 1, of shape (n_sol, *). Axis 0 holds
+            solutions, other axes are for the solution dimensions.
 
     Returns:
-        np.ndarray: 1-dimensional array.
+        np.ndarray: 1-dimensional array of shape (n_sol,).
     """
     sum_axes = tuple(range(1, np.ndim(x)))  # All axes but the first
     return np.square(x).sum(axis=sum_axes)
 
 
-def spherical_sinc(x: Union[ArrayLike, Scalar]) -> np.ndarray:
+def spherical_sinc(x: ArrayLike) -> np.ndarray:
+    """Sine cardinal of distance to 0, vectorized on axis 0.
+
+    Args:
+        x (ArrayLike): Array of dimension at least 1, of shape (n_sol, *). Axis 0 holds
+            solutions, other axes are for the solution dimensions.
+
+    Returns:
+        np.ndarray: 1-dimensional array of shape (n_sol,).
+    """
     sum_axes = tuple(range(1, np.ndim(x)))  # All axes but the first
     return np.sinc(np.sqrt(np.square(x).sum(axis=sum_axes)))
 
 
-def nsinc(x: Union[ArrayLike, Scalar]) -> np.ndarray:
+def nsinc(x: ArrayLike) -> np.ndarray:
+    """N-dimensional sine cardinal, vectorized on axis 0, summed over all other axes.
+
+    Args:
+        x (ArrayLike): Array of dimension at least 1, of shape (n_sol, *). Axis 0 holds
+            solutions, other axes are for the solution dimensions.
+
+    Returns:
+        np.ndarray: 1-dimensional array of shape (n_sol,).
+    """
     sum_axes = tuple(range(1, np.ndim(x)))  # All axes but the first
     return np.sinc(x).sum(axis=sum_axes)
